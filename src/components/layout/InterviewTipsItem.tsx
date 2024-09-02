ecab5f1c9b64605300947d104346e773c45b486d
KarlVvv <vivarkarl10@gmail.com>
2024-09-02 16:39:28 +0800

    Added icon

--- src/components/layout/InterviewTipsItem.tsx
+++ src/components/layout/InterviewTipsItem.tsx
@@ -1,5 +1,6 @@
 import { InterviewTipsItemProps } from "../../types/props";
 import PaddedContainer from "./PaddedContainer";
+import { IconFlagFilled } from "@tabler/icons-react";
 
 import styles from "./InterviewTipsItem.module.scss";
 
@@ -10,6 +11,11 @@ export default function InterviewTipsItem({
   internName,
   tipDescription,
 }: InterviewTipsItemProps) {
+  const handleIconClick = () => {
+    //click handling logic
+    console.log("Icon clicked!");
+  };
+
   return (
     <div className={`${styles.container} ${classNames}`} style={{ ...style }}>
       <PaddedContainer classNames={styles.paddedContainer}>
@@ -19,6 +25,15 @@ export default function InterviewTipsItem({
             <p className={styles.internName}>{internName}</p>
           </div>
           <p className={styles.jobDescription}>{tipDescription}</p>
+          <div>
+            <button onClick={handleIconClick} className={styles.iconButton}>
+              <IconFlagFilled
+                size={30}
+                stroke={1.5}
+                className={styles.iconReport}
+              />
+            </button>
+          </div>
         </div>
       </PaddedContainer>
     </div>
