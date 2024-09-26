f1f88203bece469da7d34181633b6044f9507452
kigos21 <83705521+kigos21@users.noreply.github.com>
2024-09-26 08:20:14 +0800

    create job management components

--- src/pages/company/CompanyManageInformationJobs.tsx
+++ src/pages/company/CompanyManageInformationJobs.tsx
@@ -2,8 +2,31 @@ import CompanyJobInformationFormItem from "../../components/ui/company/CompanyJo
 import Button from "../../components/ui/Button";
 
 import styles from "./CompanyManageInformationJobs.module.scss";
+import CompanyJobs from "../../components/ui/company/CompanyJobs";
+
+export interface Job {
+  name: string;
+  description: string;
+}
 
 export default function CompanyManageInformationJobs() {
+  const jobs: Job[] = [
+    {
+      name: "Java Developer",
+      description:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem iusto ducimus aliquam cupiditate est sapiente sequi, pariatur sint rerum magni at totam blanditiis nulla reprehenderit fugit numquam? Quasi nisi sint, iste exercitationem cum ipsum nemo enim pariatur natus nobis, qui, est temporibus provident! Sunt corporis itaque animi dolores nobis ipsum!",
+    },
+    {
+      name: "C++ Developer",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Error tempore eveniet temporibus unde, commodi exercitationem magnam nihil dolorum cupiditate? Minima praesentium ex similique obcaecati voluptatem minus pariatur molestias expedita impedit!",
+    },
+    {
+      name: "React Developer",
+      description: "Short description",
+    },
+  ];
+
   return (
     <div className={styles.container}>
       <div className={styles.titleContainer}>
@@ -16,11 +39,13 @@ export default function CompanyManageInformationJobs() {
           Add Job
         </Button>
       </div>
+
       <CompanyJobInformationFormItem
         jobTitle=""
-        tags=""
         jobDescription=""
       ></CompanyJobInformationFormItem>
+
+      <CompanyJobs jobs={jobs} />
     </div>
   );
 }
