5684071c2b1d8122c7b0aa2a86815531dfa443ee
KarlVvv <vivarkarl10@gmail.com>
2024-10-19 20:42:04 +0800

    more optimizations

--- src/pages/company/UserCompanyInterviewTips.tsx
+++ src/pages/company/UserCompanyInterviewTips.tsx
@@ -6,6 +6,15 @@ export default function UserCompanyInterviewTips() {
   return (
     <PaddedContainer classNames={styles.paddedContainer}>
       <h2>Interview Tips</h2>
+      <div className={styles.informationContainer}>
+        <p>
+          Welcome to the Interview Tips and Guidance Page. Here, you'll find
+          valuable insights and practical advice from former interns and
+          industry professionals to help you excel in your interviews. Explore
+          comprehensive strategies and best practices to enhance your
+          preparation and boost your confidence.
+        </p>
+      </div>
       <div className={styles.boxContainer}>
         <InterviewTipsItem
           subjectHeading="Good Experience"
