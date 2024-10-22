51b268675a9927c71458ba83effe554fab3738c2
kigos21 <83705521+kigos21@users.noreply.github.com>
2024-10-22 12:05:04 +0800

    add post discussion forum

--- src/pages/company/UserCompanyDiscussionForum.tsx
+++ src/pages/company/UserCompanyDiscussionForum.tsx
@@ -3,8 +3,32 @@ import DiscussionForumItem from "../../components/ui/company/DiscussionForumItem
 import Button from "../../components/ui/Button";
 import StyledBox from "../../components/layout/StyledBox";
 import styles from "./UserCompanyDiscussionForum.module.scss";
+import { useState } from "react";
+import DiscussionAddPostForm from "../../components/ui/company/DiscussionAddPostForm";
 
 export default function UserCompanyDiscussionForum() {
+  const [isAddingPost, setIsAddingPost] = useState<boolean>(false); // To control the form visibility
+  const [postForm, setPostForm] = useState({
+    description: "",
+  });
+
+  const handleSave = () => {
+    console.log("Post Saved");
+    console.log(JSON.stringify(postForm, null, 2));
+    // Add logic to handle the save
+    setIsAddingPost(false);
+    setPostForm({ description: "" });
+  };
+
+  const handleChange = (updatedPost: { description: string }) => {
+    setPostForm(updatedPost);
+  };
+
+  const handleCancel = () => {
+    setIsAddingPost(false);
+    setPostForm({ description: "" });
+  };
+
   return (
     <PaddedContainer classNames={styles.paddedContainer}>
       <div className={styles.container}>
@@ -15,16 +39,27 @@ export default function UserCompanyDiscussionForum() {
               color="primary"
               roundness="rounded"
               classNames={styles.replyButton}
+              onClick={() => setIsAddingPost(true)} // Show form on button click
             >
               Add Post
             </Button>
           </div>
+
+          {isAddingPost && (
+            <DiscussionAddPostForm
+              post={postForm}
+              onSave={handleSave}
+              onChange={handleChange}
+              onCancel={handleCancel}
+            />
+          )}
+
           <div className={styles.discussionContainer}>
             <DiscussionForumItem
               internName="John Doe"
               date="01/27/2024"
               discussionForumDescription="i love the experience i love the experience i love the experience i love the experience i love the experience"
-            ></DiscussionForumItem>
+            />
           </div>
         </div>
         <div className={styles.rightcontainer}>
