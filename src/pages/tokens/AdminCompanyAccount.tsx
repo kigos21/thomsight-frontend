61bf097326920a6297ca2affbe49f3c5370637c3
Qiroo <valdecanasjustin@gmail.com>
2024-11-09 01:00:06 +0800

    initial render of company accounts

--- src/pages/tokens/AdminCompanyAccount.tsx
+++ src/pages/tokens/AdminCompanyAccount.tsx
@@ -1,11 +1,34 @@
+import { useEffect, useState } from "react";
 import PaddedContainer from "../../components/layout/PaddedContainer";
 import StyledBox from "../../components/layout/StyledBox";
 import CompanyAccountsItem from "../../components/ui/tokens/CompanyAccountsItem";
 import SortButton from "../../components/ui/tokens/SortButton";
+import { User } from "../../types/types";
 
 import styles from "./AdminCompanyAccount.module.scss";
+import axiosInstance from "../../services/axiosInstance";
 
 export default function AdminCompanyAccount() {
+  const [repUsers, setRepUsers] = useState<User[]>([]);
+  const [loading, setLoading] = useState<string>("");
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchRepUsers = async () => {
+      setLoading("Fetching company accounts...");
+      try {
+        const response = await axiosInstance.get("/api/users/role/rep");
+        setRepUsers(response.data);
+      } catch (err) {
+        setError("Failed to fetch users");
+        console.error(err);
+      } finally {
+        setLoading("");
+      }
+    };
+
+    fetchRepUsers();
+  }, []);
   return (
     <PaddedContainer>
       <div className={styles.title}>
@@ -19,12 +42,20 @@ export default function AdminCompanyAccount() {
       <StyledBox classNames={styles.styledbox}>
         <div className={styles.companytokens}>
           <div className={styles.header}>
-            <p className={styles.token}>Token</p>
+            <p className={styles.token}>Company</p>
             <p className={styles.status}>Status</p>
             <p className={styles.expiresIn}>Expires In</p>
             <p className={styles.company}>Company</p>
           </div>
-          <CompanyAccountsItem
+          {repUsers.map((user) => (
+            <CompanyAccountsItem
+              key={user.id}
+              token="X7pL9kFg"
+              status="active"
+              expiration="1 month"
+            />
+          ))}
+          {/* <CompanyAccountsItem
             token="X7pL9kFg"
             status="active"
             expiration="1 month"
@@ -73,7 +104,7 @@ export default function AdminCompanyAccount() {
             token="X7pL9kFg"
             status="active"
             expiration="1 month"
-          />
+          /> */}
         </div>
       </StyledBox>
     </PaddedContainer>
