c5462cc8de743ef8b0dbfd88ca3db957e6ead108
Qiroo <valdecanasjustin@gmail.com>
2024-11-08 21:04:41 +0800

    request cancel working

--- src/pages/cv/CVMyRequests.tsx
+++ src/pages/cv/CVMyRequests.tsx
@@ -5,11 +5,21 @@ import { useEffect, useState } from "react";
 import axiosInstance from "../../services/axiosInstance";
 import { CV } from "../../types/types";
 import Spinner from "../../components/ui/Spinner";
+import DeletePopUp from "../../components/ui/company/DeletePopUp";
+import { useOutletContext } from "react-router-dom";
 
 const CVMyRequests = () => {
+  const { setSuccess } = useOutletContext<{
+    setSuccess: (message: string) => void;
+  }>();
+  const { setError } = useOutletContext<{
+    setError: (message: string) => void;
+  }>();
   const [requestedCVs, setRequestedCVs] = useState<CV[]>([]);
   const [reviewedCVs, setReviewedCVs] = useState<CV[]>([]);
   const [loading, setLoading] = useState<string>("Fetching CVs...");
+  const [showDeletePopup, setShowDeletePopup] = useState<boolean>(false);
+  const [selectedCVId, setSelectedCVId] = useState<number | null>(null);
 
   useEffect(() => {
     const fetchCVs = async () => {
@@ -17,10 +27,10 @@ const CVMyRequests = () => {
         const requestedResponse = await axiosInstance.get("/api/requested-cvs");
         setRequestedCVs(requestedResponse.data);
         const reviewedResponse = await axiosInstance.get("/api/reviewed-cvs");
-        console.log(reviewedResponse);
         setReviewedCVs(reviewedResponse.data);
       } catch (error) {
-        console.error("Error fetching CVs:", error);
+        setError("Error fetching CVs:");
+        console.error(error);
       } finally {
         setLoading("");
       }
@@ -29,18 +39,47 @@ const CVMyRequests = () => {
     fetchCVs();
   }, []);
 
+  const handleDelete = async () => {
+    if (!selectedCVId) return;
+    try {
+      setLoading("Cancelling request...");
+      await axiosInstance.post(`/api/${selectedCVId}/cancel-request`);
+      setSuccess("Request cancelled successfully");
+      setRequestedCVs(requestedCVs.filter((cv) => cv.id !== selectedCVId));
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading("");
+      setShowDeletePopup(false);
+    }
+  };
+
   return (
     <div className={styles.rootContainer}>
       {loading && <Spinner message={loading} />}
       {requestedCVs.map((cv) => (
-        <CVCard
-          key={cv.id}
-          name={cv.name}
-          fileTitle={cv.file}
-          description={cv.description}
-          buttonVariant="cancel"
-          onButtonClick={() => console.log(`Cancel request for ${cv.file}`)}
-        />
+        <>
+          <CVCard
+            key={cv.id}
+            name={cv.name}
+            fileTitle={cv.file}
+            description={cv.description}
+            buttonVariant="cancel"
+            onButtonClick={() => {
+              setSelectedCVId(cv.id);
+              setShowDeletePopup(true);
+            }}
+          />
+          {showDeletePopup && (
+            <DeletePopUp
+              isVisible={showDeletePopup}
+              onClose={() => setShowDeletePopup(false)}
+              onDelete={handleDelete}
+              heading="Cancel Review Request"
+              details="Are you sure you want to cancel your review request?"
+            />
+          )}
+        </>
       ))}
       {reviewedCVs.map((cv) => (
         <CVCard
