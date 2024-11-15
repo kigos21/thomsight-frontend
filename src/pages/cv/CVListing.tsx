ba777616746fb185d771e6f78f5cb6c3db43665f
Qiroo <valdecanasjustin@gmail.com>
2024-11-15 08:03:38 +0800

    delete cv pop up

--- src/pages/cv/CVListing.tsx
+++ src/pages/cv/CVListing.tsx
@@ -5,10 +5,13 @@ import { CV } from "../../types/types";
 import axiosInstance from "../../services/axiosInstance";
 import Spinner from "../../components/ui/Spinner";
 import { toast } from "react-toastify";
+import DeletePopUp from "../../components/ui/company/DeletePopUp";
 
 const CVListing = () => {
   const [cvs, setCvs] = useState<CV[]>([]);
   const [loading, setLoading] = useState<string>("Fetching CVs...");
+  const [isDeletePopupVisible, setDeletePopupVisible] = useState(false);
+  const [cvToDelete, setCvToDelete] = useState<number | null>(null);
 
   useEffect(() => {
     const fetchCVs = async () => {
@@ -25,6 +28,11 @@ const CVListing = () => {
     fetchCVs();
   }, []);
 
+  const confirmDeleteCV = (cvId: number) => {
+    setCvToDelete(cvId);
+    setDeletePopupVisible(true);
+  };
+
   const handleRequestAccess = async (cvId: number) => {
     try {
       setLoading("Requesting access...");
@@ -37,18 +45,23 @@ const CVListing = () => {
     }
   };
 
-  const handleDeleteCV = async (cvId: number) => {
+  const handleDeleteCV = async () => {
+    if (cvToDelete === null) return;
     try {
       setLoading("Deleting CV...");
-      const response = await axiosInstance.delete(`/api/cv/${cvId}/delete`);
+      const response = await axiosInstance.delete(
+        `/api/cv/${cvToDelete}/delete`
+      );
       console.log(response);
-      setCvs((prevCvs) => prevCvs.filter((cv) => cv.id !== cvId));
+      setCvs((prevCvs) => prevCvs.filter((cv) => cv.id !== cvToDelete));
       toast.success("CV deleted successfully.");
     } catch (error: any) {
       toast.error("Failed to delete CV.");
       console.error(error);
     } finally {
       setLoading("");
+      setDeletePopupVisible(false);
+      setCvToDelete(null);
     }
   };
 
@@ -64,10 +77,18 @@ const CVListing = () => {
           description={cv.description}
           buttonVariant={cv.mine ? "delete" : "request-access"}
           onButtonClick={() =>
-            cv.mine ? handleDeleteCV(cv.id) : handleRequestAccess(cv.id)
+            cv.mine ? confirmDeleteCV(cv.id) : handleRequestAccess(cv.id)
           }
         />
       ))}
+
+      <DeletePopUp
+        isVisible={isDeletePopupVisible}
+        onClose={() => setDeletePopupVisible(false)}
+        onDelete={handleDeleteCV}
+        heading="Delete CV"
+        details="Are you sure you want to delete this CV?"
+      />
     </div>
   );
 };
